bfd73c6d2ed742e6cacb5cdffc2add382b69386e
Dominic Joseph <dtpjoseph@gmail.com>
2017-08-20 00:16:26 -0700

    feat(movies): Add a movie creation endpoint

---
+++ test/plugins/features/movies/index.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Movies = require('../../../../lib/server');
+
+describe('movies integration', () => {
+
+  describe('create', () => {
+
+    it('creates a movie', () => {
+      return Movies.inject({
+        url: '/movies',
+        method: 'POST',
+        payload: { title: 'Dom the Bom Episode 6: Return of the Dominator' }
+      })
+      .then((response) => {
+        expect(response.statusCode).to.eql(200);
+        expect(response.result.object).to.eql('movie');
+      });
+    });
+
+  });
+
+});
