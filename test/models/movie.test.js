30bb938aa00125abe3f6210fd669ec3c21a1e2ab
Dominic Joseph <dtpjoseph@gmail.com>
2017-08-28 16:09:26 -0700

    feat(movies): add filter function to movie model

--- test/models/movie.test.js
+++ test/models/movie.test.js
@@ -1,8 +1,83 @@
 'use strict';
 
+const Knex  = require('../../lib/libraries/knex');
 const Movie = require('../../lib/models/movie');
 
+const oldMovie  = { release_year: 1950, name: 'Lob Interns 1950' };
+const newMovie  = { release_year: 2017, name: 'Lob Interns 2017' };
+const bestMovie = { release_year: 2016, name: 'Lob Interns 2016' };
+
 describe('movie model', () => {
+
+  describe('filter', () => {
+
+    beforeEach(() => {
+      return Knex('movies').truncate()
+      .then(() => Knex('movies').insert([oldMovie, newMovie, bestMovie]));
+    });
+
+    it('ignores an empty filter', () => {
+      return new Movie()
+      .filter({})
+      .fetchAll()
+      .then((results) => expect(results.length).to.equal(3));
+    });
+
+    it('filters by title', () => {
+      return new Movie()
+      .filter({ name: 'Interns 20' })
+      .fetchAll()
+      .then((results) => {
+        expect(results.length).to.equal(2);
+        expect(results.models[0].get('name')).to.equal('Lob Interns 2017');
+        expect(results.models[1].get('name')).to.equal('Lob Interns 2016');
+      });
+    });
+
+    it('filters within inclusive year range', () => {
+      return new Movie()
+      .filter({
+        release_year: {
+          lte: 2017,
+          gte: 2015
+        }
+      })
+      .fetchAll()
+      .then((results) => {
+        expect(results.length).to.equal(2);
+        expect(results.models[0].get('name')).to.equal('Lob Interns 2017');
+        expect(results.models[1].get('name')).to.equal('Lob Interns 2016');
+      });
+    });
+
+    it('filters within exclusive year range', () => {
+      return new Movie()
+      .filter({
+        release_year: {
+          lt: 2017,
+          gt: 2015
+        }
+      })
+      .fetchAll()
+      .then((results) => {
+        expect(results.length).to.equal(1);
+        expect(results.models[0].get('name')).to.equal('Lob Interns 2016');
+      });
+    });
+
+    it('filters for a specific year', () => {
+      return new Movie()
+      .filter({
+        release_year: 2016
+      })
+      .fetchAll()
+      .then((results) => {
+        expect(results.length).to.equal(1);
+        expect(results.models[0].get('name')).to.equal('Lob Interns 2016');
+      });
+    });
+
+  });
 
   describe('serialize', () => {
 
